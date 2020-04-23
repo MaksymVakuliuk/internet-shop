3a0d08b636a6c8d4624b816d24c71a86d30b0c58
Maksym Vakuliuk <vakuliukmaksym@gmail.com>
2020-04-23 20:57:41 +0300

    added test to UserDao in Application, implemented UserDaoImpl and UserServiceImpl

--- src/main/java/com/internet/shop/Application.java
+++ src/main/java/com/internet/shop/Application.java
@@ -2,7 +2,9 @@ package com.internet.shop;
 
 import com.internet.shop.lib.Injector;
 import com.internet.shop.model.Product;
+import com.internet.shop.model.User;
 import com.internet.shop.service.ProductService;
+import com.internet.shop.service.UserService;
 import java.util.List;
 
 public class Application {
@@ -10,15 +12,13 @@ public class Application {
 
     public static void main(String[] args) {
         ProductService productService = (ProductService) injector.getInstance(ProductService.class);
+        UserService userService = (UserService) injector.getInstance(UserService.class);
 
-        initializeBd(productService);
-        List<Product> products = productService.getAll();
-        for (Product product : products) {
-            System.out.println(product.toString());
-        }
+        testProductDao(productService);
+        testUserDao(userService);
     }
 
-    private static void initializeBd(ProductService productService) {
+    private static void testProductDao(ProductService productService) {
         Product product1 = new Product("Apple", 10.0);
         Product product2 = new Product("Orange", 15.0);
         Product product3 = new Product("Banana", 29.0);
@@ -30,5 +30,30 @@ public class Application {
         Product newProduct = new Product("Apple", 12.2);
         newProduct.setId(1L);
         productService.update(newProduct);
+
+        List<Product> products = productService.getAll();
+        for (Product product : products) {
+            System.out.println(product.toString());
+        }
     }
+
+    private static void testUserDao(UserService userService) {
+        User user1 = new User("people1", "people@1", "pass1");
+        User user2 = new User("people2", "people@2", "pass2");
+        User user3 = new User("people3", "people@3", "pass3");
+        userService.create(user1);
+        userService.create(user2);
+        userService.create(user3);
+        userService.delete(2L);
+        userService.create(new User("people4", "people@4", "pass4"));
+        User newUser = new User("newPeople", "people@newPeople", "newPass");
+        newUser.setId(1L);
+        userService.update(newUser);
+
+        List<User> users = userService.getAll();
+        for (User user : users) {
+            System.out.println(user.toString());
+        }
+    }
+
 }
--- src/main/java/com/internet/shop/dao/impl/UserDaoImpl.java
+++ src/main/java/com/internet/shop/dao/impl/UserDaoImpl.java
@@ -1,34 +1,43 @@
 package com.internet.shop.dao.impl;
 
 import com.internet.shop.dao.UserDao;
+import com.internet.shop.db.Storage;
 import com.internet.shop.lib.Dao;
 import com.internet.shop.model.User;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
 
 @Dao
 public class UserDaoImpl implements UserDao {
     @Override
     public User create(User user) {
-        return null;
+        return Storage.addUser(user);
     }
 
     @Override
-    public User get(Long id) {
-        return null;
+    public Optional<User> get(Long id) {
+        return Storage.users
+                .stream()
+                .filter(u -> u.getId().equals(id))
+                .findFirst();
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        return Storage.users;
     }
 
     @Override
     public User update(User user) {
-        return null;
+        IntStream.range(0, Storage.users.size())
+                .filter(i -> Storage.users.get(i).getId().equals(user.getId()))
+                .forEach(i -> Storage.users.set(i, user));
+        return user;
     }
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        return Storage.users.removeIf(u -> u.getId().equals(id));
     }
 }
