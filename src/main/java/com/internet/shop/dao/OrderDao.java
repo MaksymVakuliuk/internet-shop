8e139d68dfa4c65828b921f1006cdc5c301593e8
Maksym Vakuliuk <vakuliukmaksym@gmail.com>
2020-04-26 17:15:37 +0300

    refactored Dao and Service and Application

--- src/main/java/com/internet/shop/dao/OrderDao.java
+++ src/main/java/com/internet/shop/dao/OrderDao.java
@@ -1,20 +1,7 @@
 package com.internet.shop.dao;
 
 import com.internet.shop.model.Order;
-import com.internet.shop.model.User;
-import java.util.List;
-import java.util.Optional;
 
-public interface OrderDao {
-    Order create(Order order);
+public interface OrderDao extends GenericDao<Order, Long> {
 
-    List<Order> getUserOrders(User user);
-
-    Optional<Order> get(Long id);
-
-    List<Order> getAll();
-
-    Order update(Order order);
-
-    boolean delete(Long id);
 }
--- src/main/java/com/internet/shop/dao/ShoppingCartDao.java
+++ src/main/java/com/internet/shop/dao/ShoppingCartDao.java
@@ -1,17 +1,7 @@
 package com.internet.shop.dao;
 
 import com.internet.shop.model.ShoppingCart;
-import java.util.List;
-import java.util.Optional;
 
-public interface ShoppingCartDao {
-    ShoppingCart create(ShoppingCart shoppingCart);
+public interface ShoppingCartDao extends GenericDao<ShoppingCart, Long> {
 
-    Optional<ShoppingCart> get(Long id);
-
-    List<ShoppingCart> getAll();
-
-    ShoppingCart update(ShoppingCart shoppingCart);
-
-    boolean delete(Long id);
 }
