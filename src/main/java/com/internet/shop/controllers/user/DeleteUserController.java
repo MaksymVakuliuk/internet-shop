8e254ff942bf8f85906168eeb40698dcb35c663d
Maksym Vakuliuk <vakuliukmaksym@gmail.com>
2020-04-28 10:53:16 +0300

    Fixed code style in DeleteUsersController and RegestrationController. Added all products page.

--- src/main/java/com/internet/shop/controllers/user/DeleteUserController.java
+++ src/main/java/com/internet/shop/controllers/user/DeleteUserController.java
@@ -2,19 +2,19 @@ package com.internet.shop.controllers.user;
 
 import com.internet.shop.lib.Injector;
 import com.internet.shop.service.UserService;
-
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class DeleteUserController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("com.internet.shop");
     private UserService userService = (UserService) injector.getInstance(UserService.class);
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         String userID = req.getParameter("userID");
         userService.delete(Long.valueOf(userID));
         resp.sendRedirect(req.getContextPath() + "/users/all");
--- src/main/java/com/internet/shop/controllers/user/RegistrationController.java
+++ src/main/java/com/internet/shop/controllers/user/RegistrationController.java
@@ -3,24 +3,25 @@ package com.internet.shop.controllers.user;
 import com.internet.shop.lib.Injector;
 import com.internet.shop.model.User;
 import com.internet.shop.service.UserService;
-
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class RegistrationController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("com.internet.shop");
     private UserService userService = (UserService) injector.getInstance(UserService.class);
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/views/users/registration.jsp").forward(req, resp);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         String name = req.getParameter("name");
         String login = req.getParameter("login");
         String pwd = req.getParameter("pwd");
