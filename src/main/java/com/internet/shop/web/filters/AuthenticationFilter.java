1f76d56cfdca763d86506e372d7596d127d91e3b
Maksym Vakuliuk <vakuliukmaksym@gmail.com>
2020-04-30 18:00:28 +0300

    Filter added

---
+++ src/main/java/com/internet/shop/web/filters/AuthenticationFilter.java
@@ -0,0 +1,46 @@
+package com.internet.shop.web.filters;
+
+import com.internet.shop.lib.Injector;
+import com.internet.shop.service.UserService;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AuthenticationFilter implements Filter {
+    private static final String USER_ID = "userID";
+    private static final Injector INJECTOR = Injector.getInstance("com.internet.shop");
+    private static UserService userService = (UserService) INJECTOR.getInstance(UserService.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        Long userID = (Long) req.getSession().getAttribute(USER_ID);
+        if (userID == null || userService.get(userID) == null) {
+            resp.sendRedirect("/users/authentication");
+            return;
+        }
+
+        filterChain.doFilter(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
