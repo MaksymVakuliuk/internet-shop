1088cfb75308d73f670511214b8991210eb70bd8
Maksym Vakuliuk <vakuliukmaksym@gmail.com>
2020-05-04 18:24:46 +0300

    Improved implementation of GetAllOrdersOfUserController

--- src/main/java/com/internet/shop/controllers/orders/GetAllOrdersOfUserController.java
+++ src/main/java/com/internet/shop/controllers/orders/GetAllOrdersOfUserController.java
@@ -2,10 +2,11 @@ package com.internet.shop.controllers.orders;
 
 import com.internet.shop.lib.Injector;
 import com.internet.shop.model.Order;
+import com.internet.shop.model.User;
 import com.internet.shop.service.OrderService;
+import com.internet.shop.service.UserService;
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,14 +16,14 @@ public class GetAllOrdersOfUserController extends HttpServlet {
     private static final Injector INJECTOR = Injector.getInstance("com.internet.shop");
     private final OrderService orderService =
             (OrderService) INJECTOR.getInstance(OrderService.class);
+    private final UserService userService = (UserService) INJECTOR.getInstance(UserService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         Long userID = (Long) req.getSession().getAttribute("userID");
-        List<Order> orders = orderService.getAll().stream()
-                .filter(order -> order.getUser().getId().equals(userID))
-                .collect(Collectors.toList());
+        User user = userService.get(userID);
+        List<Order> orders = orderService.getUserOrders(user);
         req.setAttribute("orders", orders);
         req.getRequestDispatcher("/WEB-INF/views/orders/all.jsp").forward(req, resp);
     }
