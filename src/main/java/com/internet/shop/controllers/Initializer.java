2db20b1b754480bc94265098eed140da0c5856f9
MaksymVakuliuk <35505693+MaksymVakuliuk@users.noreply.github.com>
2020-05-12 12:48:53 +0300

    hw-21 (#11)
    * File init_db.sql creted. Pakage util with class ConnectionUtil created.
    
    * Chendeg Injector logic. Implemented ProductDaoJdbcImpl
    
    * Coments deleted
    
    * Deleted dublicate code in ProductDaoJdbcImpl

--- src/main/java/com/internet/shop/controllers/Initializer.java
+++ src/main/java/com/internet/shop/controllers/Initializer.java
@@ -1,10 +1,8 @@
 package com.internet.shop.controllers;
 
 import com.internet.shop.lib.Injector;
-import com.internet.shop.model.Product;
 import com.internet.shop.model.Role;
 import com.internet.shop.model.User;
-import com.internet.shop.service.ProductService;
 import com.internet.shop.service.UserService;
 import java.util.Set;
 import javax.servlet.ServletContextEvent;
@@ -14,18 +12,9 @@ public class Initializer implements ServletContextListener {
     private static final Injector INJECTOR = Injector.getInstance("com.internet.shop");
     private final UserService userService =
             (UserService) INJECTOR.getInstance(UserService.class);
-    private final ProductService productService =
-            (ProductService) INJECTOR.getInstance(ProductService.class);
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-        var product1 = new Product("Apple", 10.0);
-        var product2 = new Product("Orange", 15.0);
-        var product3 = new Product("Banana", 29.0);
-        productService.create(product1);
-        productService.create(product2);
-        productService.create(product3);
-
         var admin = new User("admin", "admin", "admin");
         admin.setRoles(Set.of(Role.of("ADMIN")));
         userService.create(admin);
