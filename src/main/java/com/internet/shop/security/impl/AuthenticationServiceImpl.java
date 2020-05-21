970d68bc91a4a9e9d67abaefdfec432ed8b5cce4
MaksymVakuliuk <35505693+MaksymVakuliuk@users.noreply.github.com>
2020-05-21 12:22:01 +0300

    Add password hashing function (#13)
    * Add password heshing function
    
    * Fixed code style
    
    * Add field salt to "users" table

--- src/main/java/com/internet/shop/security/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/internet/shop/security/impl/AuthenticationServiceImpl.java
@@ -6,6 +6,7 @@ import com.internet.shop.lib.Service;
 import com.internet.shop.model.User;
 import com.internet.shop.security.AuthenticationService;
 import com.internet.shop.service.UserService;
+import com.internet.shop.util.HashUtil;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
@@ -16,7 +17,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     public User login(String login, String password) throws AuthenticationException {
         User user = userService.findByLogin(login).orElseThrow(() ->
                 new AuthenticationException("Incorrect login or password."));
-        if (user.getPassword().equals(password)) {
+        if (HashUtil.isValidPassword(user.getPassword(), password, user.getSalt())) {
             return user;
         }
         throw new AuthenticationException("Incorrect login or password.");
