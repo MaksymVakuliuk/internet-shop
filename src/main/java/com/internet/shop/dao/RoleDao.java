7abe9e9497b53d5558a5dd1f1230bcb1e9d41208
Maksym Vakuliuk <vakuliukmaksym@gmail.com>
2020-08-20 17:43:06 +0300

    Removed RoleDao and RoleServise

--- src/main/java/com/internet/shop/dao/RoleDao.java
+++
@@ -1,7 +0,0 @@
-package com.internet.shop.dao;
-
-import com.internet.shop.model.Role;
-
-public interface RoleDao extends GenericDao<Role, Long> {
-
-}
--- src/main/java/com/internet/shop/service/RoleService.java
+++
@@ -1,7 +0,0 @@
-package com.internet.shop.service;
-
-import com.internet.shop.model.Role;
-
-public interface RoleService extends GenericService<Role, Long> {
-
-}
